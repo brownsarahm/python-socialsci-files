8b574b5aa3b80d1e3e6c7b81142b4fbe75a0224d
Sarah Brown <sarahmbrown@berkeley.edu>
2018-06-14 15:48:21 -0700

    update plot ex

--- code/plots.py
+++ code/plots.py
@@ -1,3 +1,8 @@
 # Make a scatter plot of 'A11_years_farm' vs 'B16_years_liv' and color the points
 # by 'C05_buildings_in_compound'
-# 
+#
+
+
+# Make a bar plot of the mean number of rooms per wall type
+
+# extension: try by wall and roof type?
