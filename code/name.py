ed60b8d0a299b7f7727c949ab356df7245d1b86f
Sarah Brown <sarahmbrown@berkeley.edu>
2018-09-03 15:28:33 -0400

    new function exercise

---
+++ code/name.py
@@ -0,0 +1,4 @@
+
+1. Write a function that accepts strings for first and last names and returns a user name that is the first letter of the first name and the whole last name.
+
+2. Supposing that in addition to the user name you also need the full name for display. Is it better to have two separate functions or I write a single function to provide both values together?
