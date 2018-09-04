0f5bc523251ec8746a811cf7dd08c7d8cf64d4e8
Sarah Brown <sarahmbrown@berkeley.edu>
2018-09-04 07:57:08 -0400

    initial exercies

---
+++ challenge/morning_warmup.py
@@ -0,0 +1,5 @@
+1.  a) create a variable 'firstname' and one 'surname' and one 'age'
+    b) assign values to them
+    c) now put them together into one list
+2. create a sequence with numbers from 10 to 100 in steps of 20
+(you can check the help for range function with help(range))
