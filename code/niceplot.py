cfbf86fce82a4684e220554325422cfe10ef98e5
Sarah Brown <sarahmbrown@berkeley.edu>
2019-03-27 08:21:21 -0400

    prep nsbe45

---
+++ code/niceplot.py
@@ -0,0 +1,8 @@
+Revisit your favorite plot we've made so far or make one with your own data.  Make the following modifications to the plot.
+
+1. add axes labels
+2. add a title
+3. add a legend
+4. save it in two different formats
+
+_Extension_: Imagine this is a type of figure that you want to be able to reproduce often for different datasets or over different variables in this dataset and write function that creates and saves the figure with labels, title and legend.
