34fa6396806dea29a9a4f5eef1837f244c6789cb
Sarah Brown <sarah_m_brown@brown.edu>
2020-08-20 09:40:44 -0400

    Update hist.py

---
+++ code/hist.py
@@ -0,0 +1,6 @@
+Use the help functions of a Jupyter Notebook or Python interpreter to explore the possible parameter settings for
+the `hist()` function in `pandas. Try to find the parameter settings that allow you to:
+
+1. Plot a separate `years_liv` histogram for each town.
+1. Make the image larger.
+1. Make the x-axis shared
